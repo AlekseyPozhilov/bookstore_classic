e4e341a547ff35d557987621b1999b559f022d2b
Aleksey Pozhilov <lpozhilov17@mail.ru>
2023-12-10 19:51:20 +0300

    Add class PropertiesManager

---
+++ src/com/belhard/bookstore/PropertiesManager.java
@@ -0,0 +1,22 @@
+package com.belhard.bookstore;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesManager {
+    private final Properties properties;
+
+    public PropertiesManager(String fileName) {
+        try (FileInputStream fis = new FileInputStream(fileName)) {
+            properties = new Properties();
+            properties.load(fis);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getKey(String key){
+        return properties.getProperty(key);
+    }
+}
