00d6a2ebe786a16e65502880552e879fac363b5c
Aleksey Pozhilov <lpozhilov17@mail.ru>
2024-01-20 00:31:16 +0300

    Edeted connection

--- src/main/java/com/belhard/bookstore/connection/ConnectionPool.java
+++ src/main/java/com/belhard/bookstore/connection/ConnectionPool.java
@@ -12,22 +12,22 @@ import java.util.concurrent.LinkedBlockingDeque;
 
 @Log4j2
 public class ConnectionPool {
-    private static final String URL = "jdbc:postgresql://127.0.0.1:5432/bookstore_pozhilov";
-    private static final String USR = "postgres";
-    private static final String PSW = "root";
     private final int poolSize;
     private final BlockingDeque<ProxyConnection> freeConnections;
     private final Queue<ProxyConnection> givenConnections;
 
-    public ConnectionPool(int poolSize, BlockingDeque<ProxyConnection> freeConnections, Queue<ProxyConnection> givenConnections) {
+    public ConnectionPool(String URL, String USR, String PSW, String DRV, int poolSize) {
         this.poolSize = poolSize;
         this.freeConnections = new LinkedBlockingDeque<>(this.poolSize);
         this.givenConnections = new ArrayDeque<>();
+
         try {
             Class.forName("org.postgresql.Driver");
             log.info("Database driver loaded");
             for (int i = 0; i < this.poolSize; i++) {
                 Connection connection = DriverManager.getConnection(URL, USR, PSW);
+                freeConnections.offer(new ProxyConnection(connection, this));
+                log.info("ConnectionPool create connection");
             }
         } catch (SQLException | ClassNotFoundException e) {
             log.error(e.getMessage(), e);
--- src/main/java/com/belhard/bookstore/connection/DataSourceImpl.java
+++ src/main/java/com/belhard/bookstore/connection/DataSourceImpl.java
@@ -2,32 +2,45 @@ package com.belhard.bookstore.connection;
 
 import lombok.extern.log4j.Log4j2;
 
+
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 @Log4j2
 public class DataSourceImpl implements DataSource {
-    private  final String url ;
+    private final String url;
     private final String user;
-    private final String password ;
+    private final String password;
     private final String drv;
-    public DataSourceImpl(String url, String user, String password, String drv) {
+    private int pooolSize;
+    private ConnectionPool connectionPool;
+
+    public DataSourceImpl(String url, String user, String password, String drv, int poolSize) {
         this.url = url;
         this.user = user;
         this.password = password;
         this.drv = drv;
+        connectionPool = new ConnectionPool(url, user, password, drv, poolSize);
     }
-    public Connection getConnection(){
+
+    public Connection getConnection() {
         try {
-            log.info("Сonnection to the database is being established...");
+            log.info("Connection to the database is being established...");
             Class.forName("org.postgresql.Driver");
-            return DriverManager.getConnection(url, user, password);
-        } catch (SQLException e) {
+            if (connectionPool == null){
+                connectionPool = new ConnectionPool(url, user, password, drv, pooolSize);
+            }
+            return connectionPool.getConnection();
+        }  catch (ClassNotFoundException e) {
             log.error("Error with connection");
             throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void close() {
+        if (connectionPool != null) {
+            connectionPool.destroyPool();
+            connectionPool = null;
         }
     }
 }
